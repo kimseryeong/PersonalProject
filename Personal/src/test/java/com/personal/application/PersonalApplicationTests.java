4ab75ec4d9d3ea3d75ae7ce9bc92dac690373e67
kimseryeong <kxxoo322@gmail.com>
2023-09-25 10:07:17 +0900

    프로젝트 생성

---
+++ Personal/src/test/java/com/personal/application/PersonalApplicationTests.java
@@ -0,0 +1,13 @@
+package com.personal.application;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PersonalApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
